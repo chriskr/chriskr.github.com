be89a72671d8f8c8f0e266930461dec2f11bae16
Christian Krebs <chrisk@opera.com>
2012-09-17 08:16:06 +0200

    Added files.

---
+++ created_index.py
@@ -0,0 +1,63 @@
+import os
+INDEX = """<!doctype html>
+<html>
+<title>idex</title>
+<style>
+body
+{
+    font-family: "Lucida Sans Unicode", sans-serif;
+    font-size: .8em;
+}
+ul
+{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+li
+{
+    padding-left: 0;
+}
+a
+{
+    text-decoration: none;
+}
+icon
+{
+    display: inline-block;
+    background-repeat: no-repeat;
+    vertical-align: middle;
+    width: -o-skin;
+    height: -o-skin;
+    margin-right: 3px;
+}
+.directory icon
+{
+    background-image: -o-skin('Folder');
+}
+.file icon
+{
+    background-image: -o-skin('Window Document Icon');
+}
+</style>
+<ul>
+"""
+
+ITEM_DIR = """<li class="directory"><a href="./%s/"><icon></icon>%s</a></li>"""
+ITEM_FILE = """<li class="file"><a href="./%s"><icon></icon>%s</a></li>"""
+BLACKLISt = [".git"]
+
+def create_index():
+    for root, dirs, files in os.walk("."):
+        with open(os.path.join(root, "index.html"), "wb") as fp:
+            fp.write(INDEX)
+            fp.write("<ul>")
+            for dir_n in dirs:
+                if not dir_n in BLACKLISt:
+                    fp.write(ITEM_DIR % (dir_n, dir_n))
+            for file_n in files:
+                if file_n.endswith(".htnl"):
+                    fp.write(ITEM_FILE % (file_n, file_n))
+            fp.write("</ul>")
+
+if __name__ == "__main__": create_index()
