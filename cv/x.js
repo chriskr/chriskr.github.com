18b8841210d23dabefab5916d8b381d76c9b0732
Chris <chrisk@opera.com>
2017-02-11 14:23:05 +0100

    Updated cv

--- cv/x.js
+++ cv/x.js
@@ -8,8 +8,11 @@
   for (let i = 8; i < s.length; i += 2) r.push(i);
   const x = r.map(i => f(p(s.slice(i, i + 2)) ^ m[((i - 8) / 2) % 4])).join('');
   const a = document.querySelector('#x');
-  a.textContent = x;
   a.href = 'mailto:' + x;
   a.removeAttribute('id');
   document.querySelector('script').remove();
+  for (let dt of Array.from(document.querySelectorAll('dt'))) {
+    dt.innerHTML =
+        Array.from(dt.textContent.trim()).map(n => `<b>${n}</b>`).join('');
+  }
 })();
