0b2ee095d401301bb7b20b738472ba20e9d1f681
Christian Krebs <christian.krebs.pi@gmail.com>
2017-09-28 16:13:19 +0200

    Fixed formats

--- sbb_clock/src/main.js
+++ sbb_clock/src/main.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-	try {
-		SBBClock.create(document.body);
-	} catch(e) {
+  try {
+    SBBClock.create(document.body);
+  } catch (e) {
     if (location.pathname.indexOf('es2015') === -1) {
       location.href = '../es2015/index.html';
     }
-	}
+  }
 };
