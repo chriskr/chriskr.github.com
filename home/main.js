64adbfff3ce2c17e6443f5dd1130c31067abd625
Christian Krebs <christian.krebs.pi@gmail.com>
2017-11-27 23:48:54 +0100

    Working on the design

--- home/main.js
+++ home/main.js
@@ -2,4 +2,8 @@
   const h5 = document.querySelector('h5');
   const markup = Array.from(h5.textContent).join('</span><span>');
   h5.innerHTML = `<span>${markup}</span>`;
+  document.addEventListener(
+      'mouseover',
+      event => document.documentElement.classList.toggle(
+          'menu-active', event.target.closest('.menu') !== null));
 }
